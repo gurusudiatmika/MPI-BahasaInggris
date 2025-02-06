d8bb42b6cdf9e2baecbf6370e6683f7475cac34e
gurusudiatmika <43933520+gurusudiatmika@users.noreply.github.com>
2025-02-06 12:48:07 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,65 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Ro2ffCqpjB":
+        Script1();
+        break;
+      case "6RHclblq3ph":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  //load the scripts dynamically into the head of the document
+
+
+
+function add_line() {
+    
+var line = document.createElement("audio");
+    
+var head=document.getElementsByTagName('body')[0];
+    
+line.type = "audio/mp3";
+    
+line.src="";
+    
+line.id="bgSong" ;
+	
+line.autoplay = true;
+	
+line.loop = true;
+    
+head.appendChild(line);
+
+}
+
+
+
+//but we only want to add these once!
+
+
+if(document.getElementById('bgSong')==null){
+	
+add_line();
+
+var audio = document.getElementById('bgSong');
+
+audio.volume = 0.5;
+
+}
+
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/Lights(chosic.com).mp3";
+audio.load();
+
+audio.play();
+}
+
